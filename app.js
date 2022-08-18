097f5511405656ed259a7ed3dda7f051f13ffd9d
Lucas Fiorelli <fiorellilucas@gmail.com>
2022-08-18 16:22:05 -0300

    Processar criação de tarefa com Express

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require("path")
 
 app.use("/", express.static("public"))
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"))
@@ -18,6 +19,13 @@ app.get("/criar", (req, res) => {
   res.sendFile(path.join(__dirname, "criar_tarefa.html"))
 })
 
+app.post("/criar", (req, res) => {
+  let formData = req.body
+  salvarTarefa(formData).then(() => {
+    res.redirect("/")
+  })
+})
+
 app.listen(5000, () => {
   console.log("Server rodando na porta 5000")
 })
@@ -30,6 +38,19 @@ const getTarefas = () => {
         reject(err)
       } else {
         resolve(result)
+      }
+    })
+  })
+}
+
+const salvarTarefa = (formData) => {
+  const conn = require ("./db_connection")
+  return new Promise((resolve, reject) => {
+    conn.query("INSERT INTO tarefas (tarefa, data) VALUES (?, ?)", [formData["tarefa"], formData["data"]], (err, result, fields) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
       }
     })
   })
